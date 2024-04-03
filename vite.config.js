b44bb573f43a8a3615e9fad5e849e6436b7c0e68
AK14 <hooploop14@gmail.com>
2024-04-03 13:41:29 +0300

    settings for npm dev

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,18 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server:{
+      hmr:{
+          host:"192.168.56.56"
+      },
+      cors:{
+          origin:"*"
+      },
+      host:"192.168.56.56",
+      watch:{
+          usePolling:true,
+      }
+    },
     plugins: [
         laravel({
             input: [
